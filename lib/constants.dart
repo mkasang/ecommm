2bfb3876fa86c7966f761bb9f1f9c9812647c2e7
Marcott <kasangandjomarcott47@gmail.com>
2024-10-14 10:31:51 +0200

    Nav bar

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MkasColor {
+  static const primaryColor = Color.fromRGBO(39, 29, 65, 3);
+  static const contentColor = Color.fromARGB(255, 198, 192, 192);
+  static const white = Colors.white;
+  static const unselectColor = Color.fromARGB(255, 189, 189, 189);
+}
---
+++ lib/screens/nav_bar_screen.dart
@@ -0,0 +1,107 @@
+import 'package:ecommm/constants.dart';
+import 'package:flutter/material.dart';
+
+class ButtomNavBar extends StatefulWidget {
+  const ButtomNavBar({super.key});
+
+  @override
+  State<ButtomNavBar> createState() => _ButtomNavBarState();
+}
+
+class _ButtomNavBarState extends State<ButtomNavBar> {
+  int currentIndex = 2;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            currentIndex = 2;
+            List screens = const [
+              Scaffold(),
+            ];
+          });
+        },
+        shape: const CircleBorder(),
+        backgroundColor: MkasColor.primaryColor,
+        child: const Icon(
+          Icons.home,
+          color: MkasColor.white,
+          size: 35,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        elevation: 1,
+        height: 60,
+        color: Colors.white,
+        shape: const CircularNotchedRectangle(),
+        notchMargin: 10,
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  currentIndex = 0;
+                });
+              },
+              icon: Icon(
+                Icons.grid_view_outlined,
+                size: 30,
+                color: currentIndex == 0
+                    ? MkasColor.primaryColor
+                    : Colors.grey.shade400,
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  currentIndex = 1;
+                });
+              },
+              icon: Icon(
+                Icons.favorite_border_outlined,
+                size: 30,
+                color: currentIndex == 1
+                    ? MkasColor.primaryColor
+                    : Colors.grey.shade400,
+              ),
+            ),
+            const SizedBox(width: 15),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  currentIndex = 3;
+                });
+              },
+              icon: Icon(
+                Icons.shopping_cart_outlined,
+                size: 30,
+                color: currentIndex == 3
+                    ? MkasColor.primaryColor
+                    : Colors.grey.shade400,
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  currentIndex = 4;
+                });
+              },
+              icon: Icon(
+                Icons.person,
+                size: 30,
+                color: currentIndex == 4
+                    ? MkasColor.primaryColor
+                    : Colors.grey.shade400,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
