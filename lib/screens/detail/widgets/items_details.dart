11a2c6b6c7ff19dd9c2ccc46ef9dc94a6acf83a0
Marcott <kasangandjomarcott47@gmail.com>
2024-10-15 16:53:48 +0200

    Items details

--- lib/screens/detail/widgets/items_details.dart
+++ lib/screens/detail/widgets/items_details.dart
@@ -1,3 +1,4 @@
+import 'package:ecommm/constants.dart';
 import 'package:ecommm/models/product.dart';
 import 'package:flutter/material.dart';
 
@@ -19,17 +20,73 @@ class ItemsDetails extends StatelessWidget {
         ),
         const SizedBox(height: 10),
         Row(
-          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Text(
-              "\$ ${product.price}",
-              style: const TextStyle(
-                fontWeight: FontWeight.w800,
-                fontSize: 25,
-              ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "\$ ${product.price}",
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w800,
+                    fontSize: 25,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Container(
+                      width: 55,
+                      height: 25,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: MkasColor.secondaryColor,
+                      ),
+                      alignment: Alignment.center,
+                      child: Row(
+                        children: [
+                          const Icon(
+                            Icons.star,
+                            size: 14,
+                            color: MkasColor.white,
+                          ),
+                          const SizedBox(width: 3),
+                          Text(
+                            product.rate.toString(),
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: MkasColor.white,
+                              fontSize: 13,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 5),
+                    Text(
+                      product.review,
+                      style: const TextStyle(color: MkasColor.unselectColor),
+                    ),
+                  ],
+                )
+              ],
             ),
+            const Spacer(),
+            Text.rich(
+              TextSpan(
+                children: [
+                  const TextSpan(
+                    text: "Seller : ",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  TextSpan(
+                    text: product.seller,
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            )
           ],
-        )
+        ),
       ],
     );
   }
